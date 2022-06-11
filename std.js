b41ef7ac5e80aef73310b8c08fba6cdcb1e9a6b0
athulprakash007 <athull362@gmail.com>
2022-06-10 22:01:42 -0700

    js

---
+++ std.js
@@ -0,0 +1,22 @@
+stdbtnevent=()=>
+{
+
+    Name=(document.getElementById("Name").value)
+    rollno=(document.getElementById("rollno").value)
+    classs=(document.getElementById("classs").value)
+    phoneno=(document.getElementById("phoneno").value)
+
+
+
+    console.log(Name)
+    alert(Name)
+
+    console.log(rollno)
+    alert(rollno)
+
+    console.log(classs)
+    alert(classs)
+
+    console.log(phoneno)
+    alert(phoneno)
+}
